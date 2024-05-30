d5e9819a81e7bfb1b5e635781c57ddab6e051641
bitoller <bi_toller@hotmail.com>
2024-05-30 20:28:29 -0300

    refactor: contents of not found page

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -5,12 +5,17 @@ import "./style.css";
 export function PageNotFound() {
   return (
     <main>
-      <img
-        src={notFoundImg}
-        alt={"imagem de dois peixes laranja no meio de uma anêmona."}
-      />
-      <p>Você encontrou o Nemo, mas não a página que estava procurando.</p>
-      <Link to="/welcome">Retornar para a página inicial</Link>
+      <div className="img-container">
+        <img
+          className="not-found-img"
+          src={notFoundImg}
+          alt={"imagem de dois peixes laranja no meio de uma anêmona."}
+        />
+        <div className="text-container">
+          <p>Você encontrou o Nemo, mas não a página que estava procurando.</p>
+          <Link to="/welcome">Retornar para a página inicial</Link>
+        </div>
+      </div>
     </main>
   );
 }
