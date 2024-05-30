452b6e8a71742e75e8e6fb351ee416b5f8d5ae69
bitoller <bi_toller@hotmail.com>
2024-05-30 20:28:02 -0300

    feat: add components to pages

--- src/pages/TheIdea/index.jsx
+++ src/pages/TheIdea/index.jsx
@@ -1,5 +1,14 @@
+import { Header } from "../../components/Header";
+import { ImagesCarousel } from "../../components/ImagesCarousel";
 import "./style.css";
 
 export function Idea() {
-  return <main></main>;
+  return (
+    <>
+      <Header />
+      <main>
+        <ImagesCarousel />
+      </main>
+    </>
+  );
 }
