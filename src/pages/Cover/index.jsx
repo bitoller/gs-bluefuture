c6be898f51f9d1182057b0290197d5d843e657f7
bitoller <bi_toller@hotmail.com>
2024-05-30 20:28:12 -0300

    feat: add cover page

--- src/pages/Cover/index.jsx
+++ src/pages/Cover/index.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
 import "./style.css";
 
 export function Cover() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="video-container">
+        <video className="video-background" autoPlay loop muted>
+          <source
+            src="https://videos.pexels.com/video-files/3765078/3765078-hd_1280_720_30fps.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+        <Link to="/home">VAMOS MERGULHAR?</Link>
+      </div>
+    </main>
+  );
 }
