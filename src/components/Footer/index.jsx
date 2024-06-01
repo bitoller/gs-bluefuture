40fb6d43f367092db51c2ce9f47bf85c8229a9f6
bitoller <bi_toller@hotmail.com>
2024-05-31 23:00:34 -0300

    feat: add footer basis

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,45 @@
+import blueFutureLogo from "../../assets/blue-future-logo.png";
+import selectLogo from "../../assets/select-logo.png";
+import softtekLogo from "../../assets/softtek-logo.png";
+import globalPactLogo from "../../assets/global-pact-logo.png";
+import unescoLogo from "../../assets/unesco-logo.png";
+import awsLogo from "../../assets/aws-logo.png";
+import oceans20Logo from "../../assets/oceans20-logo.png";
+import { NavLink } from "react-router-dom";
 import { StyledFooter } from "./style";
 
 export function Footer() {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <div className="logo-menu-container">
+        <img
+          src={blueFutureLogo}
+          alt={"logo da blue future"}
+          className="img-fluid"
+        />
+        <nav className="custom-nav">
+          <NavLink to="/home">INÍCIO</NavLink>
+          <NavLink to="/about">CORAIS</NavLink>
+          <NavLink to="/the_idea">A IDEIA</NavLink>
+          <NavLink to="/donate">DOAÇÕES</NavLink>
+        </nav>
+      </div>
+      <div className="sponsors-container">
+        <img src={selectLogo} alt={"logo da select"} className="img-fluid" />
+        <img src={softtekLogo} alt={"logo da softtek"} className="img-fluid" />
+        <img
+          src={globalPactLogo}
+          alt={"logo do pacto global"}
+          className="img-fluid"
+        />
+        <img src={unescoLogo} alt={"logo da unesco"} className="img-fluid" />
+        <img src={awsLogo} alt={"logo da aws"} className="img-fluid" />
+        <img
+          src={oceans20Logo}
+          alt={"logo da oceans 2024 brasil"}
+          className="img-fluid"
+        />
+      </div>
+    </StyledFooter>
+  );
 }
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,3 +1,48 @@
 import styled from "styled-components";
 
-export const StyledFooter = styled.footer``;
+export const StyledFooter = styled.footer`
+  background-color: #893e3e; //obviamente trocar a cor teste
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1.5625rem;
+  padding: 1.25rem;
+  background-image: url("../../assets/wave-menu-bottom.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  height: 13rem;
+
+  .logo-menu-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.3125rem;
+  }
+
+  .custom-nav {
+    display: flex;
+    flex-direction: column;
+    gap: 0.3125rem;
+  }
+
+  .custom-nav > a {
+    transition: color 0.3s;
+  }
+
+  .custom-nav > a:hover {
+    color: var(--color-primary);
+  }
+
+  .sponsors-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 48%;
+    height: 100%;
+  }
+
+  .sponsors-container > img {
+    width: 9.375rem;
+    height: auto;
+  }
+`;
