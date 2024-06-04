e973369c79b1a064a6479db7938117711c8ef66e
bitoller <bi_toller@hotmail.com>
2024-06-03 21:57:43 -0300

    feat: add content to restoration

--- src/pages/Restoration/index.jsx
+++ src/pages/Restoration/index.jsx
@@ -1,19 +1,211 @@
+import coralReef from "../../assets/coral-reefs/coral-reef-small.png";
+import coralReefAclimation from "../../assets/coral-reefs/coral-reef-aclimation.png";
+import coralReefTransportation from "../../assets/coral-reefs/coral-reef-transportation.png";
+import coralReefResearch from "../../assets/coral-reefs/coral-reef-research.png";
+import coralReefImplantation from "../../assets/coral-reefs/coral-reef-implantation.png";
 import { Header } from "../../components/Header";
 import { ImagesCarousel } from "../../components/ImagesCarousel";
 import { BackToTopArrow } from "../../components/BackToTopArrow";
 import { Footer } from "../../components/Footer";
+import React, { useState, useEffect, useRef } from "react";
+import "intersection-observer";
 import { StyledRestoration } from "./style";
 
 export function Restoration() {
+  const [animate, setAnimate] = useState([false, false, false]);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+
+    const observerCallback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = Array.from(containerRef.current.children).indexOf(
+            entry.target
+          );
+          setAnimate((prev) => {
+            const newAnimate = [...prev];
+            newAnimate[index] = true;
+            return newAnimate;
+          });
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, options);
+    const sections = containerRef.current.children;
+    Array.from(sections).forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      Array.from(sections).forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
+
   return (
     <>
       <Header />
       <ImagesCarousel />
       <StyledRestoration>
-        <h1>eu sou a tela de replantio</h1>
+        <h1>Processo para implantar os corais</h1>
+        <p>
+          A implantação de corais é um processo de restauração de recifes de
+          coral, que visa repovoar áreas danificadas ou degradadas por meio do
+          plantio de fragmentos de coral saudáveis. Os corais são cultivados em
+          viveiros marinhos ou terrestres e, uma vez que atingem um tamanho
+          adequado, são transplantados para o ambiente marinho.
+        </p>
+        <section className="super-corals-container">
+          <h2>O que é um Super Recife?</h2>
+          <p>
+            Um super recife é uma comunidade diversificada de corais dentro de
+            um sistema de recifes que é mais resistente ou resiliente a ondas de
+            calor prejudiciais. Pesquisadores estão identificando onde os super
+            recifes estão localizados e quais são os melhores em espalhar corais
+            juvenis para outros recifes. Essas informações ajudarão a orientar
+            esforços de conservação e restauração.
+          </p>
+          <p>
+            Se as temperaturas subirem acima da zona de conforto dos corais por
+            muito tempo, as algas dentro dos corais começam a produzir toxinas.
+            Os corais envenenados então expulsam as algas, perdendo tanto sua
+            cor quanto sua fonte de alimento em um único ato. Os corais podem
+            sobreviver por algum tempo com suas reservas de energia interna ou
+            ingerindo organismos do seu ambiente. Mas se esses eventos de
+            branqueamento ocorrerem com muita frequência, eles não conseguem se
+            recuperar. "Essas ondas de calor marinhas são o equivalente
+            climático a uma bomba atômica," diz a Dra. Anne Cohen. "Os danos
+            podem ser repentinos e extensos, causando branqueamento generalizado
+            e morte de corais em um único evento. No entanto, observamos
+            comunidades de corais diversas que sobrevivem a essas ondas de calor
+            com impacto mínimo e outras que se recuperam rapidamente." Esses
+            recifes estão sobrevivendo às ondas de calor devido às condições
+            locais do oceano? Eles são geneticamente mais tolerantes ao calor?
+            Ou é apenas sorte? Abordar essas questões e incorporar suas
+            respostas aos esforços de conservação são fundamentais para melhorar
+            o prognóstico dos recifes do mundo.
+          </p>
+        </section>
+        <section
+          ref={containerRef}
+          className="restoration-container monitoring-container"
+        >
+          <div
+            className={`first-container ${animate[0] ? "animate-left" : ""}`}
+          >
+            <h3>Seleção do local de implantação</h3>
+            <div>
+              <p>
+                A seleção do local é um dos componentes mais importantes de um
+                programa de restauraçãode recifes de coral. Fatores a serem
+                considerados para locais de plantio incluem: Condições do local
+                (por exemplo, qualidade da água, atividade humana, abundância de
+                predadores de corais, corais doentes); Presença local de
+                espécies de corais sendo plantadas; Condições de energia das
+                ondas; Acessibilidade do local do berçário; Facilidade de
+                plantio;
+              </p>
+              <img
+                src={coralReef}
+                alt={
+                  "imagem de um recife de corais, alguns peixes e um mergulhador"
+                }
+              />
+            </div>
+          </div>
+          <div
+            className={`second-container ${animate[1] ? "animate-right" : ""}`}
+          >
+            <h3>Aclimatação</h3>
+            <div>
+              <img
+                src={coralReefAclimation}
+                alt={"imagem de uma fazenda de corais sendo implantada"}
+              />
+              <p>
+                Dependendo da localização do berçário (por exemplo, viveiros
+                terrestres), os corais podem precisar de um período de
+                aclimatação antes de serem plantados na natureza. A aclimatação
+                pode envolver os corais se ajustando à iluminação, temperatura
+                ou turbidez do local do outplant.
+              </p>
+            </div>
+          </div>
+          <div
+            className={`third-container ${animate[2] ? "animate-left" : ""}`}
+          >
+            <h3>Transportando os corais</h3>
+            <div>
+              <p>
+                Ao transportar corais do berçário para o local de implantação,
+                reduzir o estresse dos corais é de extrema importância. Os
+                corais devem ser transportados durante as épocas mais frias e
+                calmas do ano, não durante períodos de estresse por calor ou
+                aumento da atividade das tempestades.
+              </p>
+              <img
+                src={coralReefTransportation}
+                alt={
+                  "imagem de um mergulhador transportando uma fazenda de corais"
+                }
+              />
+            </div>
+          </div>
+          <div
+            className={`fourth-container ${animate[3] ? "animate-right" : ""}`}
+          >
+            <h3>Saúde e tamanho do transplante de corais</h3>
+            <div>
+              <img
+                src={coralReefResearch}
+                alt={"imagem de um mergulhador coletando amostras dos corais"}
+              />
+              <p>
+                Os corais não devem ser plantados se apresentarem quaisquer
+                condições anormais, como perda recente de tecido, descoloração,
+                palidez/branqueamento ou parasitas. Por causa dos custos de
+                manter os corais em um viveiro, é melhor plantar corais assim
+                que eles forem grandes o suficiente para ter uma boa chance de
+                sobrevivência após o transplante.
+              </p>
+            </div>
+          </div>
+          <div
+            className={`fifth-container ${animate[4] ? "animate-left" : ""}`}
+          >
+            <h3>Métodos de Anexos</h3>
+            <div>
+              <p>
+                Os corais são plantados, assegurando-os ao recife usando uma
+                variedade de métodos de fixação. O método de fixação deve ser
+                testado e será parcialmente determinado pelas espécies de coral
+                que estão sendo plantadas, pelo tipo de substrato no local da
+                planta e pelas condições do local. Antes de qualquer método de
+                fixação ser usado, todos os organismos incrustantes e sedimentos
+                devem ser removidos do substrato usando pequenas escovas de
+                arame e raspadores.
+              </p>
+              <img
+                src={coralReefImplantation}
+                alt={"imagem de um recife de corais que foi implantado"}
+              />
+            </div>
+          </div>
+        </section>
       </StyledRestoration>
       <BackToTopArrow />
       <Footer />
     </>
   );
 }
+
+/* TODO: tentar conter o texto dos super recifes dentro da svg. o primeiro p na esquerda e o segundo na direita? */
--- src/pages/Restoration/style.js
+++ src/pages/Restoration/style.js
@@ -1,3 +1,4 @@
+import pictureContainer from "../../assets/backgrounds/container-picture.svg";
 import styled from "styled-components";
 
 export const StyledRestoration = styled.main`
@@ -7,9 +8,185 @@ export const StyledRestoration = styled.main`
   align-items: center;
   gap: 1.875rem;
 
+  > p {
+    width: 70%;
+    min-height: 25rem;
+    background-image: url(${pictureContainer});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0.625rem 0.625rem 0.625rem 45.3125rem;
+  }
+
+  p {
+    line-height: 1.875rem;
+    font-size: 1.2rem;
+  }
+
   > h1 {
     font-size: 2.5rem;
     color: var(--color-primary);
     font-weight: 500;
+  }
+
+  .super-corals-container {
+    display: flex;
+    flex-direction: column;
+    gap: 1.875rem;
+    width: 70%;
+  }
+
+  .super-corals-container > h2 {
+    font-size: 1.875rem;
+    color: var(--color-accent);
+    font-weight: 500;
+  }
+
+  .super-corals-container > p {
+    color: var(--color-gray-4);
+  }
+
+  .restoration-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.875rem;
+    width: 100%;
+  }
+
+  .first-container,
+  .third-container,
+  .fifth-container {
+    background-color: var(--color-accent);
+    display: flex;
+    flex-direction: column;
+    gap: 1.875rem;
+    width: 70%;
+    border-radius: 10px;
+    padding: 1.25rem;
+    opacity: 0;
+    transform: translateX(0);
+    transition: transform 1s, opacity 1s;
+  }
+
+  .second-container,
+  .fourth-container {
+    background-color: var(--color-primary);
+    display: flex;
+    flex-direction: column;
+    gap: 1.875rem;
+    width: 70%;
+    border-radius: 10px;
+    padding: 1.25rem;
+    opacity: 0;
+    transform: translateX(0);
+    transition: transform 1s, opacity 1s;
+  }
+
+  .first-container > h3,
+  .second-container > h3,
+  .third-container > h3,
+  .fourth-container > h3,
+  .fifth-container > h3 {
+    font-size: 1.5625rem;
+    font-weight: 500;
+  }
+
+  .first-container > div,
+  .second-container > div,
+  .third-container > div,
+  .fourth-container > div,
+  .fifth-container > div {
+    display: flex;
+    align-items: center;
+    gap: 1.875rem;
+  }
+
+  .first-container > div > img,
+  .second-container > div > img,
+  .third-container > div > img,
+  .fourth-container > div > img,
+  .fifth-container > div > img {
+    border-radius: 5px;
+  }
+
+  .monitoring-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  .first-container.animate-left,
+  .third-container.animate-left,
+  .fifth-container.animate-left {
+    transform: translateX(0);
+    opacity: 1;
+  }
+
+  .second-container.animate-right,
+  .fourth-container.animate-right {
+    transform: translateX(0);
+    opacity: 1;
+  }
+
+  .first-container {
+    transform: translateX(-100%);
+  }
+
+  .second-container {
+    transform: translateX(100%);
+  }
+
+  .third-container {
+    transform: translateX(-100%);
+  }
+
+  .fourth-container {
+    transform: translateX(100%);
+  }
+
+  .fifth-container {
+    transform: translateX(-100%);
+  }
+
+  @media (max-width: 1490px) {
+    > p {
+      width: 90%;
+      background-image: none;
+      color: var(--color-gray-4);
+      padding: 0;
+    }
+
+    .first-container > div,
+    .second-container > div,
+    .third-container > div,
+    .fourth-container > div,
+    .fifth-container > div {
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+
+    .super-corals-container,
+    .restoration-container {
+      width: 90%;
+    }
+
+    .restoration-container > div {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 662px) {
+    .first-container > div > img,
+    .second-container > div > img,
+    .third-container > div > img,
+    .fourth-container > div > img,
+    .fifth-container > div > img {
+      width: 80%;
+    }
   }
 `;
